5a96c50c06016d94ed0c0530f85d0411002c25c1
jlcorrea-creator <jerrylimcorrea@outlook.com>
2025-05-25 17:03:49 +0800

    Create sw.js
    new sw.js file

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'offline-cache-v1';
+const ASSETS = ['/', '/index.html', '/styles.css', '/main.js'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
