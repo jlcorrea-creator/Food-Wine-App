c80b0a346d1fb99a14304e20e8c4d7ada909bc31
jlcorrea-creator <jerrylimcorrea@outlook.com>
2025-05-25 21:59:41 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+      .then(() => console.log('Service Worker Registered'))
+      .catch(err => console.error('SW registration failed:', err));
+   }
